bbb624d79d6f8cf3f8c1abeb20bafce42b1764db
Rahul Bhadoriya <rahulbh8077@gmail.com>
2026-08-11 12:41:52 +0530

    Create CheckSorted.cpp

---
+++ Sorting/CheckSorted.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Function to check if the array is sorted
+bool isSorted(int arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
+            // If any element is smaller than the previous one, return false
+            if (arr[j] < arr[i]) 
+                return false;
+        }
+    }
+
+    return true; // Return true if no unsorted elements are found
+}
+
+int main() {
+    int arr[] = {1, 2, 3, 4, 5}, n = 5;
+    bool ans = isSorted(arr, n);
+    
+    // Output result
+    if (ans) cout << "True" << endl;
+    else cout << "False" << endl;
+    
+    return 0;
+}
